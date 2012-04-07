80e69503694b1ebb32cabde5471a404bb0f48629
Steve Ottenad <sottenad@gmail.com>
2012-04-07 16:40:47 -0700

    Initial JS + XML work

--- js/script.js
+++ js/script.js
@@ -1,6 +1,44 @@
-/* Author:
+/* Author: Steve Ottenad*/
 
-*/
+/*Global Variables*/
+var slideData;
+
+$(function(){
+
+	$.ajax({
+		url: 'slides.xml',
+		crossDomain:true,
+		success: function(data){
+			var slideData = data;
+			processSlides(slideData);
+		},
+		error: function(jqXHR, textStatus, errorThrown){
+			console.log('Error:: ' + textStatus);
+		}
+	})
+	
+})
+
+function processSlides(data){
+	
+	
+	$(data).find('slide').each(function(i){	
+		var image = $(this).find('image').text();
+		var background = $(this).find('background').text();
+		var hand = $(this).find('hand').text();
+		
+		var output = '<div id="slide_'+i+'" class="outer">';
+		output += '<div class="bg" style="background:url(img/bg/'+background+')">';	
+		output += '<div class="holder" style="background:url(img/hand/'+hand+') top left">';	
+		output += '<img src="img/screens/'+image+'" />';
+		output += '</div></div></div>';
+		
+		
+		$('#cont').append(output);
+	})
+	
+	
+}
 
 
 
